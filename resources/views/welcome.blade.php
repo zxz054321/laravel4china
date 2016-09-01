7e3f86bcc7ab8afe96d1c0c6ea1d732f13ef1284
Abel <zxz054321@163.com>
2016-09-01 16:45:13 +0800

    Remove links of googleapis

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,6 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
         <style>
             html, body {
                 height: 100%;
