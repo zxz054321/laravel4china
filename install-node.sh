232cfd03bb59379cdafdfad6e4ce7261c1effacf
Abel <zxz054321@163.com>
2016-10-13 20:48:30 +0800

    Switch to Node.js v4.x LTS

--- install-node.sh
+++ install-node.sh
@@ -5,13 +5,9 @@ echo $'by Abel <zxz054321@163.com>\n'
 
 hash node 2>/dev/null && { echo "Node.js already installed."; }
 hash node 2>/dev/null || {
-    echo "Installing Node.js v5.x..."
-    curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+    echo "Installing Node.js v4.x LTS..."
+    curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
     sudo apt-get install -y nodejs
-    sudo ln -s /usr/bin/nodejs /usr/bin/node
-
-    echo "Installing Npm..."
-    sudo apt-get install npm
     npm config set registry http://registry.cnpmjs.org
 }
 
