0b097202c3faea7bff12586c01fccfc044a90831
Abel <zxz054321@163.com>
2016-10-15 12:07:11 +0800

    Autoload IdeHelperServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if($this->app->environment() !== 'production') {
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        if ($this->app->environment() !== 'production') {
+            $provider = \Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class;
+
+            if (class_exists($provider)) {
+                $this->app->register($provider);
+            }
         }
     }
 }
