5ac0ad03686fdb8c7ba5bfe33c5fe7c9b747fee1
Abel <zxz054321@foxmail.com>
2017-08-31 11:31:43 +0800

    Dump to v5.4.30 structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,12 +65,12 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    @auth
+                    @if (Auth::check())
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
                 </div>
             @endif
 
